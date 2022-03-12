ee0e47ea4fe7b970fdcae1c4bb1188d794b1fbd7
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-12 23:36:50 +0530

    created a first script for she bang demonstration

--- sample1.sh
+++ sample1.sh
@@ -1,4 +1,13 @@
 #!bin/bash
 
 echo Hello Naren
+## Color Codes
+# Violet   10
+# Indigo   11
+# Blue     12
+# Green    13
+# Yellow   14
+# Orange   15
+# Red      16
+echo -e "\e[31m Hello\e[0m"
 
