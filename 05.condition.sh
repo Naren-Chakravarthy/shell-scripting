4acf0544ddc0cc466a15403e7c504c8b129e05e8
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-14 21:38:11 +0530

    Conditons script

---
+++ 05.condition.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Conditions
+## if forms
+# 1.simple if
+# syntax if ["expression"]
+# then
+# commands
+# fi
+
+a=1
+if [a -eq 1]
+then
+  echo "Hello"
+  fi
+# 2.if else
+# syntax if
+# else if
