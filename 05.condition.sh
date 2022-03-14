35292652f7bfa77113efcc36e9bf5cc3ec3d4613
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-14 22:00:48 +0530

    Conditons script

--- 05.condition.sh
+++ 05.condition.sh
@@ -7,11 +7,28 @@
 # then
 # commands
 # fi
-a=1
-if [ "$a" -eq 1 ]
+
+if [ 1 -eq 1 ]
 then
 echo "Hello"
 fi
+##expressions are important
+#string tests
+# operators: ==, !=, -z
+
+a=naren
+if [ "$a" == naren ]; then
+  echo "Botha are equal"
+fi
+
+if [ "$a" != naren ]; then
+  echo "Both are not equal"
+fi
+
+if [ -z "$b" ]; then
+  echo b variable is empty
+fi
+
 # 2.if else
 # syntax if
 # else if
