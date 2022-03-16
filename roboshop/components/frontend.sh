83bc8871d0d221a71ebebb1c8d5aff02b6a329f4
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-17 00:54:07 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+StatCheck() {
+  if [ "$1" -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+}
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
   echo You should run your script as a root or sudo user
@@ -9,32 +16,19 @@ fi
 
 echo -e "\e[36m Installing nginx \e[0m"
 yum install nginx -y
-if [ "$?" -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+StatCheck $?
 
 
 echo -e "\e[36m Starting the nginx \e[0m "
 systemctl enable nginx
 systemctl start nginx
-if [ "$?" -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+StatCheck $?
+
 
 echo -e "\e[36m Downloading the nginx content \e[0m "
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [ "$?" -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+StatCheck $?
+
 
 echo -e "\e[36m Cleanup old nginx content and extract new downloaded archive \e[0m "
 rm -rf /usr/share/nginx/html/*
@@ -44,21 +38,11 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if [ "$?" -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+StatCheck $?
 
 
 echo -e "\e[36m Restarting the nginx \e[0m "
 systemctl restart nginx
 systemctl enable nginx
-if [ "$?" -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+StatCheck $?
 
