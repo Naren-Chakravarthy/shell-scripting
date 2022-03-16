62a5c2478489a225199fccf110e7fac1b7270a5f
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-17 01:40:47 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -16,42 +16,43 @@ if [ "$USER_ID" -ne 0 ]; then
   echo You should run your script as a root or sudo user
   exit 1
 fi
+LOG_FILE=/tmp/roboshop.log
 
 
 Print "installing nginx"
-yum install nginx -y
+yum install nginx -y >>$LOG_FILE
 StatCheck $?
 
 
 Print "Starting the nginx"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx >>$LOG_FILE
+systemctl start nginx >>$LOG_FILE
 StatCheck $?
 
 
 Print "Downloading the nginx content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
 StatCheck $?
 
 
 Print "Cleanup old nginx content"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 StatCheck $?
 
 cd /usr/share/nginx/html/
 
 
 Print "Extracting and Archive"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
 StatCheck $?
 
 
 Print "Update Roboshop Configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
 StatCheck $?
 
 
 Print "Restarting the nginx"
-systemctl restart nginx && systemctl enable nginx
+systemctl restart nginx >>$LOG_FILE && systemctl enable nginx >>$LOG_FILE
 StatCheck $?
 
