a0198fc35c1d873a9886159ca283f269ab8dfb20
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-17 01:56:06 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,7 +9,7 @@ StatCheck() {
 }
 
 Print() {
-  echo -e "\n-----------------$1-------------------" >>$LOG_FILE
+  echo -e "\n-----------------$1-------------------" &>>$LOG_FILE
   echo -e "\e[36m "$1" \e[0m"
 }
 USER_ID=$(id -u)
@@ -21,35 +21,35 @@ LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 Print "installing nginx"
-yum install nginx -y >>$LOG_FILE
+yum install nginx -y &>>$LOG_FILE
 StatCheck $?
 
 
 Print "Starting the nginx"
-systemctl enable nginx >>$LOG_FILE
-systemctl start nginx >>$LOG_FILE
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx &>>$LOG_FILE
 StatCheck $?
 
 
 Print "Downloading the nginx content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 StatCheck $?
 
 
 Print "Cleanup old nginx content"
-rm -rf /usr/share/nginx/html/* >>$LOG_FILE
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 StatCheck $?
 
 cd /usr/share/nginx/html/
 
 
 Print "Extracting and Archive"
-unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE && mv frontend-main/* . &>>$LOG_FILE && mv static/* . &>>$LOG_FILE
 StatCheck $?
 
 
 Print "Update Roboshop Configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 StatCheck $?
 
 
