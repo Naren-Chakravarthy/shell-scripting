29bd7389a7ec729fb87820e516fec00a2c585dd7
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-17 20:01:15 +0530

    mongodb setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -21,16 +21,15 @@ LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 Print "Setup mongodb repos"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 StatCheck $?
 
 Print "Installing mongodb"
-yum install -y mongodb-org
+yum install -y mongodb-org &>>$LOG_FILE
 StatCheck $?
 
 Print "Starting the mongodb"
-systemctl enable mongod
-systemctl start mongod
+systemctl enable mongod &>>$LOG_FILE && systemctl start mongod &>>$LOG_FILE
 StatCheck $?
 
 
