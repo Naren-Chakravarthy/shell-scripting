120ebe4ba231e5ffffc998f6b78ff55a5d6a8350
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-17 20:24:30 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -36,5 +36,12 @@ Print "Update mongodb listen IP Adress"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 StatCheck $?
 
+Print "Restarting the mongodb"
+systemctl restart mongod &>>$LOG_FILE
+StatCheck $?
+
+
+
+
 
 
