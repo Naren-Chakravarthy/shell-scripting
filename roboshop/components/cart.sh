6e55ca2c09e21d4a8e4d62c4c669fe19d0ddb2d6
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-19 00:08:33 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,1 +1,6 @@
-echo cart component
+#!/bin/bash
+
+source components/common.sh
+
+COMPONENT=cart
+NODEJS
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -66,6 +66,8 @@ NODEJS(){
   sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APP_USER/$COMPONENT/systemd.service &>>$LOG_FILE
   sed -i -e 's/REDIS_ENDPOINT/user.roboshop.internal/' /home/$APP_USER/$COMPONENT/systemd.service &>>$LOG_FILE
   sed -i -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/$APP_USER/$COMPONENT/systemd.service &>>$LOG_FILE
+  sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' /home/$APP_USER/$COMPONENT/systemd.service &>>$LOG_FILE
+  sed -i -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/$APP_USER/$COMPONENT/systemd.service &>>$LOG_FILE
   mv /home/$APP_USER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service &>>$LOG_FILE
   StatCheck $?
 
