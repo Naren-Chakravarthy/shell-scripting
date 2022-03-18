1b9c7b20f6f9f690a0a6132de3339b8ce19ab0d5
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-18 08:30:53 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -10,4 +10,18 @@ Print"Installing NodeJS"
 yum install nodejs gcc-c++ -y &>>$LOG_FILE
 StatCheck $?
 
+Print "Download app Component"
+curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
+StatCheck $?
 
+Print"Cleanup old content"
+rm -rf /home/roboshop/catalogue &>>$LOG_FILE
+StatCheck $?
+
+Print "Extract app Content"
+cd /home/roboshop &>>$LOG_FILE && unzip -o /tmp/catalogue.zip &>>$LOG_FILE && mv catalogue-main catalogue &>>$LOG_FILE
+StatCheck $?
+
+Print "Install app Dependencies"
+cd /home/roboshop/catalogue &>>$LOG_FILE && npm install &>>$LOG_FILE
+StatCheck $?
