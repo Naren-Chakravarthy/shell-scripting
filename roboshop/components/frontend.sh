28828ef5f9a02e0e53c1e3a354a90f3735a59195
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-20 11:18:15 +0530

    Merge remote-tracking branch 'origin/main'

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -32,11 +32,11 @@ StatCheck $?
 
 Print "Update Roboshop Configuration"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-sed -i -e '/user/s/localhost/user.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-sed -i -e '/cart/s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+for components in catalogue user cart; do
+echo -e "updating $components configuration"
+sed -i -e '/$components/s/localhost/$components.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 StatCheck $?
-
+done
 
 Print "Restarting the nginx"
 systemctl restart nginx >>$LOG_FILE && systemctl enable nginx >>$LOG_FILE
