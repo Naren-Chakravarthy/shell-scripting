38de25c48a9b14377a900253ece896b4bc85b119
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-22 20:21:37 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ roboshop/components/dispatch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source components/common.sh
+
+
+Print "Installing GoLang"
+ yum install golang -y &>>$LOG_FILE
+ StatCheck $?
+
+APP_SETUP
+
+Print "Intilization of dispatch service"
+ go mod init dispatch &>>$LOG_FILE
+ go get &>>$LOG_FILE
+ go build &>>$LOG_FILE
+ StatCheck $?
+
+
+
+
+# Update the systemd file and configure the dispatch service in systemd
