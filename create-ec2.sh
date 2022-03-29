a54bb0d09983b4dcaa495cdd1c29570c9250bb4d
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-29 17:40:48 +0530

    create ec2 instance script

--- create-ec2.sh
+++ create-ec2.sh
@@ -14,5 +14,5 @@ aws ec2 run-instances \
     --image-id ${AMI_ID} \
     --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-    --secutity-group-ids $SG_ID \
+    --security-group-ids $SG_ID \
     | jq
