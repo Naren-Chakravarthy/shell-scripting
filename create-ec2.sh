5348d2d9b3cccbc1a2ff2bf873f790710ee25f56
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-29 17:35:13 +0530

    create ec2 instance script

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,5 +8,11 @@ COMPONENT=$1
 
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow all traffic from public" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 echo $AMI_ID
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+aws ec2 run-instances \
+    --image-id ${AMI_ID} \
+    --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+    --secutity-group-ids $SG_ID \
+    | jq
