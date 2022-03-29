1bc52949102d403eba17a8da272bfbc6f1e7a3b4
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-03-29 18:16:45 +0530

    create ec2 instance script

--- create-ec2.sh
+++ create-ec2.sh
@@ -5,14 +5,18 @@ if [ -z "$1" ]; then
 fi
 
 COMPONENT=$1
+ZONE_ID="Z07840263P1VQXP880Q8W"
 
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow all traffic from public" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 echo $AMI_ID
-aws ec2 run-instances \
+PTRIVATE_IP=$(aws ec2 run-instances \
     --image-id ${AMI_ID} \
     --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-    --security-group-ids $SG_ID \
-    | jq
+    --security-group-ids ${SG_ID} \
+    | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+
